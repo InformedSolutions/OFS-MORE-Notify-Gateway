d1ae277114beedd455ca4b96e8aee39656b93e89
Cruddas <james.cruddas@informed.com>
2018-03-02 11:17:52 +0000

    settings: build.py: Use dev key to protect SMS limit

--- notify/settings/build.py
+++ notify/settings/build.py
@@ -20,4 +20,4 @@ INSTALLED_APPS = BUILTIN_APPS + THIRD_PARTY_APPS + DEV_APPS + PROJECT_APPS
 SECRET_KEY = 'bwf1%fz)rl&i7zb9hd$bkccqw-402hq=mg=h*31(w=@b+iz-8*'
 
 # Notify API team-scoped key (sends emails to whitelisted team members)
-NOTIFY_API_KEY = "dev_api-7c51af0f-8720-4315-9d67-b4f94d7531e0-df9b0c2e-6d50-4102-ae62-9a24cde656cc"
+NOTIFY_API_KEY = "test_key-7c51af0f-8720-4315-9d67-b4f94d7531e0-6ebc794f-f0d9-40c6-bd4f-013119b47c04"
