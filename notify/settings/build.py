593390bc09114137607504dab204a6617958f66d
Cruddas <cruddasj@informed.co.uk>
2018-03-12 22:14:37 +0000

    settings: Import API key via env

--- notify/settings/build.py
+++ notify/settings/build.py
@@ -11,4 +11,4 @@ INSTALLED_APPS = BUILTIN_APPS + THIRD_PARTY_APPS + PROJECT_APPS
 SECRET_KEY = 'bwf1%fz)rl&i7zb9hd$bkccqw-402hq=mg=h*31(w=@b+iz-8*'
 
 # Notify API team-scoped key (sends emails to whitelisted team members)
-NOTIFY_API_KEY = "test_key-7c51af0f-8720-4315-9d67-b4f94d7531e0-6ebc794f-f0d9-40c6-bd4f-013119b47c04"
+NOTIFY_API_KEY = os.environ.get('NOTIFY_API_KEY')
