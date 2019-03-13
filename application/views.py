b68763ca766902bfc443421a6857647be54697f4
James Cruddas <james.cruddas@informed.com>
2019-03-13 09:12:46 +0000

    Add notoify service for serious incidents

--- application/views.py
+++ application/views.py
@@ -102,6 +102,8 @@ def __send_email_via_notify(data):
             NOTIFICATIONS_CLIENT = NotificationsAPIClient(settings.PAY_NOTIFY_API_KEY)
         elif service_name == 'Nannies':
             NOTIFICATIONS_CLIENT = NotificationsAPIClient(settings.NANNIES_NOTIFY_API_KEY)
+        elif service_name == 'Serious Incidents':
+            NOTIFICATIONS_CLIENT = NotificationsAPIClient(settings.SERIOUS_INCIDENT_NOTIFY_API_KEY)
         else:
             NOTIFICATIONS_CLIENT = NotificationsAPIClient(settings.NOTIFY_API_KEY)
     else:
